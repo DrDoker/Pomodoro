a3d0db81a43e632fe1d4d61a13b49da3f4931bee
Serhii Tkachenko <doker.tk@gmail.com>
2022-08-21 01:58:48 +0200

    Add button and label. Minimal init

--- Pomodoro/Sources/App/MainViewController.swift
+++ Pomodoro/Sources/App/MainViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 import SnapKit
 
 class MainViewController: UIViewController {
-    // MARK: - ?? Outlets
+    // MARK: - Labels Outlets
+
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:00"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 45, weight: .thin)
+        label.textColor = .black
+        return label
+    }()
+
+    // MARK: - Buttons Outlets
+
+    private lazy var startStopButton: UIButton = {
+        let button = UIButton(type: .system)
+
+        let configSymbol = UIImage.SymbolConfiguration(pointSize: 50, weight: .thin, scale: .default)
+        let image = UIImage(systemName: "play", withConfiguration: configSymbol)
+
+        button.setImage(image, for: .normal)
+        button.setImage(UIImage(systemName: "stop"), for: .disabled)
+        button.tintColor = .black
+        button.clipsToBounds = true
+        return button
+    }()
 
     // MARK: - Lifecycle
 
@@ -18,17 +42,25 @@ class MainViewController: UIViewController {
         setupHierarchy()
         setupLayout()
 
-        view.backgroundColor = .yellow
+        view.backgroundColor = .systemYellow
     }
 
     // MARK: - Setup
 
     private func setupHierarchy() {
-
+        view.addSubview(timeLabel)
+        view.addSubview(startStopButton)
     }
 
     private func setupLayout() {
+        timeLabel.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
 
+        startStopButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view).offset(70)
+        }
     }
 
     // MARK: - Actions

ea9f7712e713eb1054a72f5273e9ece2122bc1b6
Serhii Tkachenko <doker.tk@gmail.com>
2022-08-20 23:28:40 +0200

    Remove storyboard and add folders

---
+++ Pomodoro/Sources/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Serhii  on 20/08/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
--- Pomodoro/Sources/App/MainViewController.swift
+++ Pomodoro/Sources/App/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Pomodoro
 //
 //  Created by Serhii  on 20/08/2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
 }
-

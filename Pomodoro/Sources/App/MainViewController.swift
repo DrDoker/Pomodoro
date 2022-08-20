2e98cdb5f0ef98449b1687c9cd7bbf9e897b9d57
Serhii Tkachenko <doker.tk@gmail.com>
2022-08-20 23:34:09 +0200

    Add MARK comments and setup func

--- Pomodoro/Sources/App/MainViewController.swift
+++ Pomodoro/Sources/App/MainViewController.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    // MARK: - ?? Outlets
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+
         view.backgroundColor = .yellow
     }
+
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+
+    // MARK: - Actions
+    
 }
